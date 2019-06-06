b18fd65ce81c4c1782c1f0e69aafdbf5b4dfbd86
JasonWuGenius <2480629967@qq.com>
2019-06-06 15:31:18 +0800

    20190606

--- BigComp_2018/stringConnect.java
+++ BigComp_2018/stringConnect.java
@@ -1,5 +1,30 @@
 package BigComp_2018;
 
-public class stringConnect {
-
+/*
+ * 	字符串连连看
+ * 	
+ * 题目描述
+	对于输入的字符串，从左到右扫描字符串，如果存在由三个以上（包括三个）连续相同字符组成的子串，就将这个子串从原串中去掉，并将原有字符串剩下的部分拼接到一起。重复上述过程，直到无法去掉任何子串
+	输入描述:
+	输入的字符串
+	输出描述:
+	最后剩下的子串
+	示例1
+	输入
+	复制
+	AAABCCDDDCB
+	输出
+	复制
+	BB
+ */
+import java.io.*;
+import java.lang.reflect.Array;
+import java.util.*;
+public class stringConnect{
+    public static void main(String[] args) throws IOException{
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        char[] chrs = bf.readLine().toCharArray();
+        
+    }
 }
+
